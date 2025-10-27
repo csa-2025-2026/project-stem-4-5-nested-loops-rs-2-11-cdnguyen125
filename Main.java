14e30bcbb86c10d7d6f251f62cf743b31e5fac15
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2025-10-27 18:22:27 +0000

    1

--- Main.java
+++ Main.java
@@ -5,16 +5,30 @@ public class Main
   public static void main(String[] args)
   {
     // Make sure to test your methods here
+    printNTimes("code", 3);
+    System.out.println();
   }
 
   public static void printNTimes(String word, int N)
   {
     // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String currentLetter = word.substring(i, i+1);
+      for (int j = 0; j < N; j++)
+      {
+        System.out.print(currentLetter);
+      }
+    }
   }
 
   public static void printNums()
   {
     // code solution here
+    for (int a = 10; a > 0 ; a--)
+    {
+       
+    }
   }
 
   public static void uprightNumberTriangle(int N)
